ee1df6a5585754ba2a3aa1a532d5d69b0fe0f4c0
dos Passos <Leonardo.dosPassos@standardbank.co.za>
2020-02-03 13:09:41 +0200

    created folder structure using cookiecutter

---
+++ noise-and-hearing-things/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a project that contains methods and functions for processing, cleaning and preparing data',
+    author='Leonardo dos Passos',
+    license='',
+)
